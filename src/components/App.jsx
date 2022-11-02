092c10b7d86337a6b88494d4c72dd67d68268f18
Dimon23114 <99199990+Dimon23114@users.noreply.github.com>
2022-11-02 21:17:32 +0200

    update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,6 +18,23 @@ export class App extends Component {
     filter: '',
   };
 
+componentDidMount(){
+const contacts = localStorage.getItem('contacts');
+const parsedContacts = JSON.parse(contacts);
+
+if(parsedContacts){
+  this.setState({contacts: parsedContacts})
+}
+ 
+}
+
+  componentDidUpdate(prevProps, prevState) {
+if(this.state.contacts !== prevState.contacts) {
+localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+}
+
+  }
+
   addContact = data => {
     if (this.isDublicate(data)) {
       return alert(
